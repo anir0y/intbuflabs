dd311514829f5887562acf99621db2ec91b3b70f
beast-machine <animeshroy@live.com>
2024-08-16 17:02:50 +0530

    fixed build script
    Signed-off-by: beast-machine <animeshroy@live.com>

--- bulild.sh
+++ bulild.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+echo "Running build script"
+echo "[+] Setting up environment"
+export GOOS=windows
+export GOARCH=amd64
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-g++
+export CGO_ENABLED=1
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-g++
+echo "[+] Building ArishtiOverflow.exe"
 GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags="-H windowsgui" -o ArishtiOverflow.exe
